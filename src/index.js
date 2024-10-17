917f8878c190841f0afd5a87475ffd216d66f23e
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-17 13:45:33 +1100

    feat: server hello world implemented

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// Make an instance of the Express system
+// So we can configure it
+// eg: routes, settings
+const app = express();
+
+// GET localhost:3000/
+// .get("/", (req, res) => {})
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello World!</h1>");
+    response.json({
+        message:"Hello World!"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on localhost:${PORT}`);
+});
